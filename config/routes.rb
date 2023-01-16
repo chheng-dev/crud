b2288ac6c9cdda1dbf3507d1aa3d549b38bc2d94
Chheng <oeungchungchheng2205@gmail.com>
2023-01-16 11:44:14 +0700

    crur

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # get 'students/index'
+  # get 'students/show'
+  # get 'students/edit'
+  # get 'students/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :students
+
+end
